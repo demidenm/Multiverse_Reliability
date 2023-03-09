1ccba0d158962d612252c8811551959e6817516d
Michael Demidenko <36335002+demidenm@users.noreply.github.com>
2023-03-08 16:53:38 -0800

    revise first level + calc fsl.SUSAN
    adding niftimasker before firstlevel computer (due to fact that FirstLevelModel applies smoothing kernel before masking) and trying to replicate susan smooth from fmri_prep calc [do not incorporate due to compute time + output style]

--- Stage1_Code/firstlevel_model_permutations_new.py
+++ Stage1_Code/firstlevel_model_permutations_new.py
@@ -4,6 +4,7 @@ from pandas import read_csv
 from glob import glob
 from Stage1_Code.designmat_regressors_define import create_design_mid, pull_regressors
 from nilearn.image import binarize_img
+from nilearn.maskers import NiftiMasker
 from nilearn.image import smooth_img
 from nilearn.glm.first_level import FirstLevelModel
 from itertools import product
@@ -13,10 +14,10 @@ proj_loc = f'{local}/Desktop/Academia/Stanford/2_F32/Multiverse_Reliability'
 
 # Model options
 fwhm = [4,5] # exclude in product for efficiency purposes
-mot = ["opt1", "opt5"]
-mod_type = ["AntMod", "FixMod"]
+mot = ["opt1"]#, "opt5"]
+mod_type = ["AntMod"]#, "FixMod"]
 mask_type = ["desc-brain_mask", "label-GM_probseg"]
-permutation_list = list(product(mot, mod_type, mask_type))
+permutation_list = list(product(fwhm, mot, mod_type, mask_type))
 
 # model permutations
 model_types = {
@@ -39,10 +40,10 @@ runs = ['run-01', 'run-02']
 
 contrasts = {
     'Lgain-Neut': 'LargeGain - NoMoneyStake',
-    'Sgain-Neut': 'SmallGain - NoMoneyStake',
-    'Lgain-Base': 'LargeGain',
-    'Sgain-Base': 'SmallGain',
-}
+    'Sgain-Neut': 'SmallGain - NoMoneyStake'}#,
+#    'Lgain-Base': 'LargeGain',
+#    'Sgain-Base': 'SmallGain',
+#}
 
 # provide TR & volumes for associated BOLD
 boldtr = .8
@@ -52,8 +53,8 @@ for subj in subjects:
     start_time = time.time()
     for run in runs:
         print(f'\tStarting {subj} {run}.')
-        for motion, model, mask_type in permutation_list:
-            print('\t\t Running model using: {}, {}, {}'.format(motion, model, mask_type))
+        for smooth, motion, model, mask_type in permutation_list:
+            print('\t\t Running model using: {}, {}, {}, {}'.format(fwhm, motion, model, mask_type))
             print('\t\t 1/4 Load Files & set paths')
             # import behavior events .tsv from data path
             events_df = read_csv(f'{sher_path}/{subj}/ses-1/func/{subj}_ses-1_task-mid_{run}_events.tsv', sep='\t')
@@ -84,35 +85,36 @@ for subj in subjects:
                 # using .05 probability to mask out dark black voxels (likely in WM)
                 mask = binarize_img(mask_path, threshold=0.05, mask_img=None)
 
-            # fitting glm for sub's run, using associated mask,
+            # masking and smoothing
+            masker = NiftiMasker(mask_img=mask_path if mask_type == 'desc-brain_mask' else mask, standardize=False)
+            bold_data = masker.fit_transform(nii_path)
+            smoothed_data = smooth_img(masker.inverse_transform(bold_data), fwhm=smooth)
+
             # using ar1 autocorrelation (FSL prewhitening), drift model
             fmri_glm = FirstLevelModel(subject_label=subj,
-                                       mask_img=mask_path if mask_type == 'desc-brain_mask' else mask,
+                                       mask_img=None,
                                        t_r=boldtr, smoothing_fwhm=None,
                                        standardize=False, noise_model='ar1', drift_model=None, high_pass=None
                                        # cosine 0:3 included from fmriprep in desing mat based on 128 s calc
                                        )
 
             # Run GLM model using set paths and calculate design matrix
-            run_fmri_glm = fmri_glm.fit(nii_path, design_matrices=design_matrix)
+            run_fmri_glm = fmri_glm.fit(smoothed_data, design_matrices=design_matrix)
 
             print('\t\t 4/4: From GLM model, create z-score contrast maps and save to output path')
             # contrast names and associated contrasts in contrasts defined is looped over
             # contrast name is used in saving file, the contrast is used in deriving z-score
             for con_name, con in contrasts.items():
-                for smooth in fwhm:
-                    beta_name = f'{firstlvl_output}/{subj}_ses-01_task-mid_{run}_contrast-{con_name}_mask-{mask_type}' \
-                                f'_mot-{motion}_mod-{model}_fwhm-{smooth}_beta.nii.gz'
-                    beta_est = run_fmri_glm.compute_contrast(con, output_type='effect_size')
-                    beta_est_smooth = smooth_img(imgs=beta_est, fwhm=smooth)
-                    beta_est_smooth.to_filename(beta_name)
+                beta_name = f'{firstlvl_output}/{subj}_ses-01_task-mid_{run}_contrast-{con_name}_mask-{mask_type}' \
+                            f'_mot-{motion}_mod-{model}_fwhm-{smooth}_beta.nii.gz'
+                beta_est = run_fmri_glm.compute_contrast(con, output_type='effect_size')
+                beta_est.to_filename(beta_name)
 
-                    # Calc: variance
-                    var_name = f'{firstlvl_output}/{subj}_ses-01_task-mid_{run}_contrast-{con_name}_mask-{mask_type}' \
-                            f'_mot-{motion}_mod-{model}_fwhm-{smooth}_var.nii.gz'
-                    var_est = run_fmri_glm.compute_contrast(con, output_type='effect_variance')
-                    var_est_smooth = smooth_img(imgs=var_est, fwhm=smooth)
-                    var_est_smooth.to_filename(var_name)
+                # Calc: variance
+                var_name = f'{firstlvl_output}/{subj}_ses-01_task-mid_{run}_contrast-{con_name}_mask-{mask_type}' \
+                        f'_mot-{motion}_mod-{model}_fwhm-{smooth}_var.nii.gz'
+                var_est = run_fmri_glm.compute_contrast(con, output_type='effect_variance')
+                var_est.to_filename(var_name)
 
     end_time = time.time()
     print('\t ** First Level Permutations Completed.'
