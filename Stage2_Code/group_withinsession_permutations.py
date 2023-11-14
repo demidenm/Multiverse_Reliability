c2d2ccbfa566cdd8f856ec354d01aa485ccf5c6a
Michael Demidenko <36335002+demidenm@users.noreply.github.com>
2023-11-14 15:10:24 -0800

    refactor group estimates

--- Stage2_Code/group_withinsession_permutations.py
+++ Stage2_Code/group_withinsession_permutations.py
@@ -42,7 +42,7 @@ def group_onesample(fixedeffect_paths: list, session: str, task_type: str,
     sec_lvl_model = sec_lvl_model.fit(second_level_input=fixedeffect_paths,
                                       design_matrix=design_matrix)
 
-    # Calculate z-statistic from second lvl map
+    # Calculate t-statistic from second lvl map
     tstat_map = sec_lvl_model.compute_contrast(
         second_level_contrast='Intercept',
         second_level_stat_type='t',
@@ -50,50 +50,33 @@ def group_onesample(fixedeffect_paths: list, session: str, task_type: str,
     )
 
     # group out file, naming subs-N
-    tstat_out = f'{group_outdir}/subs-{N_maps}_{session}_task-{task_type}_contrast-{contrast_type}' \
+    tstat_out = f'{group_outdir}/subs-{N_maps}_ses-{session}_task-{task_type}_contrast-{contrast_type}' \
                 f'_{model_permutation}_stat-tstat.nii.gz'
     tstat_map.to_filename(tstat_out)
 
 
 parser = argparse.ArgumentParser(description="Script to run first level task models w/ nilearn")
-
 parser.add_argument("--sample", help="sample type, ahrb, abcd or mls?")
-parser.add_argument("--sub", help="subject name, sub-XX, include entirety with 'sub-' prefix")
 parser.add_argument("--task", help="task type -- e.g., mid, reward, etc")
 parser.add_argument("--ses", help="session, include the session type without prefix, e.g., 1, 01, baselinearm1")
-parser.add_argument("--fmriprep_path", help="Path to the output directory for the fmriprep output")
+parser.add_argument("--model", help="model permutation, e.g. contrast-Sgain-Neut_mask-mni152_mot-opt5_mod-FixMod_fwhm-6.0")
 parser.add_argument("--mask", help="path the to the binarized brain mask (e.g., MNI152 or "
                                  "constrained mask in MNI space, or None")
 parser.add_argument("--mask_label", help="label for mask, e.g. subtresh, suprathresh, yeo-network, or None")
+parser.add_argument("--input", help="input path to data")
 parser.add_argument("--output", help="output folder where to write out and save information")
 
 args = parser.parse_args()
 
 # Now you can access the arguments as attributes of the 'args' object.
 sample = args.sample
-subj = args.sub
 task = args.task
 ses = args.ses
-fix_dir = args.fmriprep_path
 brainmask = args.mask
 mask_label = args.mask_label
+model = args.model
+fix_dir = args.input
 scratch_out = args.output
-
-# Model permutations
-if sample in ['abcd', 'ahrb']:
-    voxel = 2.4
-    opts = np.array([1.5,  2, 2.5, 3, 3.5])
-    fwhm_opt = list(np.round(voxel * opts, 2))
-elif sample in 'mls':
-    voxel = 2.4
-    inh_smooth_weight = .75
-    opts = np.array([1.5, 2, 2.5, 3, 3.5])*inh_smooth_weight
-    fwhm_opt = list(np.round(voxel * opts, 2))
-
-motion_opt = ["opt1", "opt2", "opt3", "opt4", "opt5"]
-modtype_opt = ["CueMod", "AntMod", "FixMod"]
-
-permutation_list = list(product(fwhm_opt, motion_opt, modtype_opt))
 
 # contrasts
 contrasts = [
@@ -103,12 +86,10 @@ contrasts = [
 
 for contrast in contrasts:
     print(f'\t Working on contrast map: {contrast}')
-    for fwhm, motion, model in permutation_list:
-        model = f'mask-{mask_label}_mot-{motion}_mod-{model}_fwhm-{fwhm}'
 
-        # find all contrast fixed effect maps for model permutation across subjects
-        fix_maps = sorted(glob(f'{fix_dir}/**/*_{ses}_task-{task}_effect-fixed_'
-                               f'contrast-{contrast}_{model}_stat-effect.nii.gz'))
-        group_onesample(fixedeffect_paths=fix_maps, session=ses, task_type=task,
-                        contrast_type=contrast, group_outdir=scratch_out,
-                        model_permutation=model, mask=brainmask)
+    # find all contrast fixed effect maps for model permutation across subjects
+    fix_maps = sorted(glob(f'{fix_dir}/*_{ses}_task-{task}_*'
+                           f'contrast-{contrast}_{model}_stat-beta.nii.gz'))
+    group_onesample(fixedeffect_paths=fix_maps, session=ses, task_type=task,
+                    contrast_type=contrast, group_outdir=scratch_out,
+                    model_permutation=model, mask=brainmask)
