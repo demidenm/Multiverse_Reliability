1519e5f3846385e4e75c942fcc3b1b75d0bbdaa1
Michael Demidenko <demidenm@umich.edu>
2023-11-17 16:12:14 -0800

    revised ahrb batch

--- Stage2_Code/cluster_jobs/make_ahrb-sbatch.sh
+++ Stage2_Code/cluster_jobs/make_ahrb-sbatch.sh
@@ -2,7 +2,9 @@
 
 subj_ids=$1
 ses=1
-sample=ahrb
+sample=AHRB
+task=mid
+inp_path=/oak/stanford/groups/russpold/data/${sample}
 
 if [ -z "$1" ]; then
 	echo
@@ -14,10 +16,10 @@ fi
 n=0
 cat $subj_ids | while read line ; do
 
-	sed -e "s|SUBJECT|${line}|g; s|SESSION|${ses}|g" ./templates/${sample}_firstlvl.txt > ./batch_${sample}/first${n}
+	sed -e "s|SUBJECT|${line}|g; s|SESSION|${ses}|g; s|TASK|${task}|g; s|DATA_LOC|${inp_path}|g; s|SAMPLE|${sample}|g;" ./templates/${sample,,}_firstlvl.txt > ./batch_jobs/first${n}
 	n=$((n+1))
 
 done
 
-chmod +x ./batch_${sample}/first*
+chmod +x ./batch_jobs/first*
 
