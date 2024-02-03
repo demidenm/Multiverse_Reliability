607ed3e1115c3050517b897ae625c7ebe17f3afd
Michael Demidenko <demidenm@umich.edu>
2024-02-02 18:33:48 -0600

    modified abcd template/batch subm

--- Stage2_Code/cluster_jobs/make_abcd-first.sh
+++ Stage2_Code/cluster_jobs/make_abcd-first.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 subj_ids=$1
+acomp_list=$1 # tsv file, no header, col1 = sub IDs w/ sub- prefix and col2 acompcor exclusion "1" non-exclusion "0"
 ses=baselineYear1Arm1
 out_dir=/scratch.global/${USER}/analyses_reliability
 
 if [ -z "$1" ]; then
 	echo
-	echo "Error: Missing list. Provide subject list w/o 'sub-' prefix in positon 1."
+	echo "Error: Missing list. Provide subject list w 'sub-' prefix in positon 1."
 	echo
 	exit 1
 fi
 
 n=0
 cat $subj_ids | while read line ; do
-
-	sed -e "s|SUBJECT|${line}|g; s|SESSION|${ses}|g; s|OUTPUT|${out_dir}|g;" ./templates/abcd_firstlvl.txt > ./batch_jobs/first${n}
+	subj=$(echo $line | awk -F" " '{ print $1 }' | awk -F"-" '{ print $2 }')
+	sed -e "s|SUBJECT|${subj}|g; s|SESSION|${ses}|g; s|OUTPUT|${out_dir}|g; s|ACOMPCOR|${acomp_list}|g;" ./templates/abcd_firstlvl.txt > ./batch_jobs/first${n}
 	n=$((n+1))
 
 done
--- Stage2_Code/cluster_jobs/make_abcd-group.sh
+++ Stage2_Code/cluster_jobs/make_abcd-group.sh
@@ -3,12 +3,27 @@
 curr_dir=`pwd`
 sample=abcd # abcd, ahrb or mls
 task=MID
+run=1 # 1, 2 or None (no need for leading 0, added in code
 ses=baselineYear1Arm1 # baselineYear1Arm1 or 2YearFollowUpArm1 for ABCD
-type=run # run or session
-subj_list=${curr_dir}/subj_ids/abcd_ids.txt
-inpfold=/scratch.global/${USER}/analyses_reliability/firstlvl
+type=session # run or session
+subj_list=${1}
 outfold=/scratch.global/${USER}/analyses_reliability/group
 
+if [ -z "$1" ]; then
+        echo
+	echo "Error: Missing list. Provide subject list w 'sub-' prefix in positon 1."
+        echo
+	exit 1
+fi
+
+# if using runs, inp should be firstlvl; if using sessions, inp should be fixedeff
+if [ ${type} == 'session' ]; then
+	inpfold=/scratch.global/${USER}/analyses_reliability/fixedeff
+elif [ ${type} == 'run' ]; then
+	inpfold=/scratch.global/${USER}/analyses_reliability/firstlvl
+else
+                echo "Invalid type: $type"
+fi
 
 # Model permutations
 if [[ $sample == 'abcd' || $sample == 'ahrb' ]]; then
@@ -17,7 +32,7 @@ elif [[ $sample == 'mls' ]]; then
     fwhm_opt=(3.6 4.8 6.0 7.2 8.4)
 fi
 
-motion_opt=("opt1" "opt2" "opt3" "opt4" "opt5")
+motion_opt=("opt1" "opt2" "opt3" "opt4") # "opt5")
 modtype_opt=("CueMod" "AntMod" "FixMod")
 
 # Start loop to create ICC batch jobs
@@ -26,7 +41,7 @@ for fwhm in ${fwhm_opt[@]} ; do
 	for motion in ${motion_opt[@]} ; do
 		for modtype in ${modtype_opt[@]} ; do
 			model="mask-mni152_mot-${motion}_mod-${modtype}_fwhm-${fwhm}"
-			sed -e "s|MODEL|${model}|g; s|SESSION|${ses}|g; s|TASK|${task}|g; s|TYPE|${type}|g;  s|INPUT|${inpfold}|g; s|OUTPUT|${outfold}|g; s|SUBJ_IDS|${subj_list}|g; s|SAMPLE|${sample}|g;" ./templates/group.txt > ./batch_jobs/group${n}
+			sed -e "s|MODEL|${model}|g; s|RUN|${run}|g; s|SESSION|${ses}|g; s|TASK|${task}|g; s|TYPE|${type}|g;  s|INPUT|${inpfold}|g; s|OUTPUT|${outfold}|g; s|SUBJ_IDS|${subj_list}|g; s|SAMPLE|${sample}|g;" ./templates/group.txt > ./batch_jobs/group${n}
         		n=$((n+1))
 	        done
     	done
